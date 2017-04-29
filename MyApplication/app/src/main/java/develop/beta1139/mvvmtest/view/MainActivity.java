6c5667fb2a4fd92bae22104efdc4a996aa4891c3
tomo1139 <tomo1139@gmail.com>
2017-04-30 00:06:02 +0900

    observable to list

--- MyApplication/app/src/main/java/develop/beta1139/mvvmtest/view/MainActivity.java
+++ MyApplication/app/src/main/java/develop/beta1139/mvvmtest/view/MainActivity.java
@@ -3,14 +3,18 @@ package develop.beta1139.mvvmtest.view;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+
+import java.util.List;
 
 import develop.beta1139.mvvmtest.R;
 import develop.beta1139.mvvmtest.databinding.ActivityMainBinding;
 import develop.beta1139.mvvmtest.eventbus.Publisher;
 import develop.beta1139.mvvmtest.viewmodel.MainViewModel;
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         binding.setMainViewModel(mainViewModel);
 
         EventBus.getDefault().register(this);
+
+        Observable<String> observable = Observable.just("aaa", "bbb");
+        List<String> list = observable.toList().blockingGet();
+        for (String str : list) {
+            Log.e("dbg", "str: " + str);
+        }
     }
 
     @Subscribe
