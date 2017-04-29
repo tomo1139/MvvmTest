5f0fe478592f8048d1b1074009c4a0eb66447faf
tomo1139 <tomo1139@gmail.com>
2017-04-29 13:29:41 +0900

    onClickのバインディング追加

--- MyApplication/app/src/main/java/develop/beta1139/mvvmtest/viewmodel/MainViewModel.java
+++ MyApplication/app/src/main/java/develop/beta1139/mvvmtest/viewmodel/MainViewModel.java
@@ -1,6 +1,7 @@
 package develop.beta1139.mvvmtest.viewmodel;
 
 import android.databinding.ObservableField;
+import android.view.View;
 
 /**
  * Created by tomo on 2017/04/29.
@@ -11,5 +12,9 @@ public class MainViewModel {
     public ObservableField<String> name = new ObservableField<>("xxx");
 
     public MainViewModel() {
+    }
+
+    public void onClickButton(View view) {
+        name.set("yyy");
     }
 }
